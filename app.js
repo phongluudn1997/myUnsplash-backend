73256bdd7eb331a584ea02a7545a015828535c1d
Phong Luu <huynhpluu@gmail.com>
2020-10-08 23:12:07 +0700

    added: route config

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv/config");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api", require("./routes"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
