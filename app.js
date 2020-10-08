998b345d19777b42d1b893e26ec482a631156e06
Phong Luu <huynhpluu@gmail.com>
2020-10-08 23:38:26 +0700

    added: connect to database

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv/config");
 
+const connectDB = require("./helpers/db-connection");
+connectDB();
+
 app.use("/api", require("./routes"));
 
 const port = process.env.PORT || 3000;
---
+++ helpers/db-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL;
+
+const connectDB = () => {
+  mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Connection error:"));
+  db.once("open", () => {
+    console.log("Database connected successfully!");
+  });
+};
+
+module.exports = connectDB;
