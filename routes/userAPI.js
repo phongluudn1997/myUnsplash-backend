b3172a9420531b058161fe5626709133632fb75f
Phong Luu <luu.huynh@fiisoft.com>
2020-10-09 09:22:04 +0700

    added: user login function

--- routes/userAPI.js
+++ routes/userAPI.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 const SALT = 10;
+const privateKey = process.env.PRIVATE_KEY || "";
 
 router.post("/register", async (req, res, next) => {
-  const {
-    email,
-    password,
-    nickname
-  } = req.body;
+  const { email, password, nickname } = req.body;
 
   try {
     const existedUser = await User.findOne({
-      email
+      email,
     }).exec();
     if (existedUser) {
       return res.status(400).json({
@@ -22,14 +20,14 @@ router.post("/register", async (req, res, next) => {
       });
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
 
   const hashedPassword = await bcrypt.hash(password, SALT);
   const newUser = new User({
     email,
     password: hashedPassword,
-    nickname
+    nickname,
   });
   newUser
     .save()
@@ -41,6 +39,51 @@ router.post("/register", async (req, res, next) => {
       });
     })
     .catch((err) => next(err));
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Email and password required!",
+    });
+  }
+
+  try {
+    const user = await User.findOne({
+      email,
+    }).exec();
+    if (!user) {
+      return res.status(400).json({
+        message: "The email hasn't been registered!",
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({
+        message: "Wrong password!",
+      });
+    }
+
+    try {
+      const token = await jwt.sign(
+        {
+          userId: user._id,
+        },
+        privateKey
+      );
+      return res.status(200).json({
+        message: "Login successfully!",
+        token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
