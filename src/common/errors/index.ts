b89a0a7640f1366cf6c28176add4850b668ae558
Phong Luu <huynhpluu@gmail.com>
2020-10-11 14:48:55 +0700

    added: class for not found error

--- src/common/errors/index.ts
+++ src/common/errors/index.ts
@@ -1,4 +1,4 @@
 export * from "./custom-error";
-export * from "./bad-request-error";
-export * from "./request-validations-error"
+export * from "./bad-request.error";
+export * from "./request-validations.error"
 
---
+++ src/common/errors/not-found.error.ts
@@ -0,0 +1,14 @@
+import CustomError from "./custom-error";
+
+export class NotFoundError extends CustomError {
+    statusCode: number = 404;
+    serializeErrors(): { message: string; field?: string; }[] {
+        return [{
+            message: "API not found"
+        }]
+    }
+    constructor(){
+        super("API not found")
+    }
+
+}
