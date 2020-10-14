997ff8d9dfec420e823247dd3b63f328f6a3f22c
Phong Luu <huynhpluu@gmail.com>
2020-10-14 19:48:07 +0700

    removed: unused

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,32 +1,10 @@
 import bcrypt from "bcrypt";
 import { IUserRegisterDTO } from "../interfaces/IUser";
 import BadRequestError from "../common/errors/bad-request.error";
-import { User } from "../schemas/user";
-import userModel from "schemas/user/user.model";
 import { Models } from "../../@types/express";
 import { randomBytes } from "crypto";
 
 const SALT = 10;
-
-export const register = async (
-  email: string,
-  password: string,
-  nickname: string
-) => {
-  const existedUser = await User.findOne({ email }).exec();
-  if (existedUser) {
-    throw new BadRequestError("Email registered!");
-  }
-
-  const hashedPassword = await bcrypt.hash(password, SALT);
-  const user = await User.create({
-    email,
-    password: hashedPassword,
-    nickname,
-  });
-  return user;
-};
-
 export default class UserService {
   userModel: Models.UserModel;
   constructor(userModel: Models.UserModel) {
